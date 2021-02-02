d6dea4f07b1039bddfabbdabcedb9650ba9e3009
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-02 13:45:46 +0300

    feat: add app-store-scraper

--- packages/store/src/routes/index.js
+++ packages/store/src/routes/index.js
@@ -1,43 +1,42 @@
 import * as Fastify from 'fastify'
-
-// import search from './search.js'
+import search from './search.js'
 
 /**
  * @param {Fastify.FastifyInstance} f
  */
 export default function routes(f, _opts, done) {
-  // f.route({
-  //   method: 'GET',
-  //   url: '/search',
-  //   preHandler: null,
-  //   schema: {
-  //     querystring: {
-  //       type: 'object',
-  //       properties: {
-  //         keyword: { type: 'string' },
-  //         country_id: { type: 'string' },
-  //       },
-  //       required: ['keyword'],
-  //     },
-  //     response: {
-  //       200: {
-  //         type: 'array',
-  //         items: {
-  //           type: 'object',
-  //           properties: {
-  //             id: { type: 'number' },
-  //             bundle: { type: 'string' },
-  //             title: { type: 'string' },
-  //             url: { type: 'string' },
-  //             developer_id: { type: 'string' },
-  //             developer_name: { type: 'string' },
-  //           },
-  //         },
-  //       },
-  //     },
-  //   },
-  //   handler: search,
-  // })
+  f.route({
+    method: 'GET',
+    url: '/search',
+    preHandler: null,
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          keyword: { type: 'string' },
+          country_id: { type: 'string' },
+        },
+        required: ['keyword'],
+      },
+      response: {
+        200: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'number' },
+              bundle: { type: 'string' },
+              title: { type: 'string' },
+              url: { type: 'string' },
+              developer_id: { type: 'string' },
+              developer_name: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
+    handler: search,
+  })
 
   done()
 }
