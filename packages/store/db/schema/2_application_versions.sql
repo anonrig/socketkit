9a6b5b8d7bdb3677496931029b27e5e17d3ab18d
Emre Hasegeli <emre@hasegeli.com>
2021-02-03 10:43:43 +0300

    Fix db schemas

--- packages/store/db/schema/2_application_versions.sql
+++ packages/store/db/schema/2_application_versions.sql
@@ -3,8 +3,8 @@ SET ROLE store;
 CREATE TABLE application_versions (
   score int NOT NULL,
   reviews int NOT NULL,
-  released_at datetime NOT NULL,
-  updated_at datetime NOT NULL,
+  released_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL,
   price numeric(2) NOT NULL,
   release_notes text NOT NULL,
   application_id text NOT NULL,
@@ -17,11 +17,11 @@ CREATE TABLE application_versions (
   required_os_version text NOT NULL,
   store_url text NOT NULL,
   currency_id text NOT NULL,
-  languages text[] NOT NULL ARRAY[],
+  languages text[] NOT NULL DEFAULT '{}',
   screenshots json NOT NULL,
   website text,
   content_rating text NOT NULL,
-  required_os_version text NOT NULL,
 
-  PRIMARY KEY (application_id, country_id, version)
+  PRIMARY KEY (application_id, country_id, version),
+  FOREIGN KEY (application_id) REFERENCES applications
 );
--- packages/store/db/schema/3_application_ratings.sql
+++ packages/store/db/schema/3_application_ratings.sql
@@ -3,9 +3,8 @@ SET ROLE store;
 CREATE TABLE application_ratings (
   application_id text NOT NULL,
   country_id text NOT NULL,
-  rating_histogram int[] NOT NULL DEFAULT ARRAY[],
+  rating_histogram int[] NOT NULL DEFAULT '{}',
 
   PRIMARY KEY (application_id, country_id),
-  FOREIGN KEY (country_id) REFERENCES countries,
   FOREIGN KEY (application_id) REFERENCES applications
 );
