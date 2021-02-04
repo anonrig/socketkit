19cb807f18cade45338d1882cf075557f7501c4d
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-04 14:51:58 +0300

    feat: implement findOne and findVersions

--- packages/store/src/consumers/application/index.js
+++ packages/store/src/consumers/application/index.js
@@ -13,9 +13,17 @@ export async function findOne({
       application_id: 'applications.application_id',
       developer_id: 'applications.developer_id',
       bundle_id: 'applications.bundle_id',
-      application_title: 'application_versions.title',
-      application_description: 'application_versions.description',
+      title: 'application_versions.title',
+      description: 'application_versions.description',
+      release_notes: 'application_versions.release_notes',
+      icon: 'application_versions.icon',
+      store_url: 'application_versions.store_url',
+      languages: 'application_versions.languages',
+      screenshots: 'application_versions.screenshots',
+      version: 'application_versions.version',
       ratings: 'application_versions.rating_histogram',
+      released_at: 'application_versions.released_at',
+      updated_at: 'application_versions.updated_at',
     })
     .from('applications')
     .innerJoin('application_versions', function () {
@@ -47,13 +55,36 @@ export async function findVersions({ application_id }) {
   return pg
     .queryBuilder()
     .select({
-      // application_id: 'applications.application_id',
-      // developer_id: 'applications.developer_id',
-      // bundle_id: 'applications.bundle_id',
+      application_id: 'applications.application_id',
+      developer_id: 'applications.developer_id',
+      bundle_id: 'applications.bundle_id',
+      title: 'application_versions.title',
+      description: 'application_versions.description',
+      release_notes: 'application_versions.release_notes',
+      icon: 'application_versions.icon',
+      store_url: 'application_versions.store_url',
+      languages: 'application_versions.languages',
+      screenshots: 'application_versions.screenshots',
+      version: 'application_versions.version',
+      ratings: 'application_versions.rating_histogram',
+      released_at: 'application_versions.released_at',
+      updated_at: 'application_versions.updated_at',
     })
     .from('application_versions')
+    .innerJoin('applications', function () {
+      this.on(
+        'application_versions.application_id',
+        'applications.application_id',
+      )
+    })
     .where(function () {
-      this.where('application_versions.application_id', application_id)
+      if (application_id) {
+        this.where('applications.application_id', application_id)
+      }
+
+      if (bundle_id) {
+        this.where('applications.bundle_id', bundle_id)
+      }
     })
     .orderBy('application_versions.released_at', 'desc')
 }
