131ace053295d620d13fe1b8ffaa2951310028b7
Emre Hasegeli <emre@hasegeli.com>
2021-02-04 09:35:19 +0300

    Add last_fetch column to applications

--- packages/store/db/schema/1_applications.sql
+++ packages/store/db/schema/1_applications.sql
@@ -1,13 +1,19 @@
 SET ROLE store;
 
 CREATE TABLE applications (
+  created_at timestamptz NOT NULL DEFAULT now(),
+  last_fetch timestamptz NOT NULL DEFAULT now(),
   application_id text NOT NULL,
   developer_id text NOT NULL,
   bundle_id text NOT NULL,
 
   PRIMARY KEY (application_id),
   UNIQUE (bundle_id),
-  FOREIGN KEY (developer_id) REFERENCES developers
+  FOREIGN KEY (developer_id) REFERENCES developers,
+  CONSTRAINT applications_last_fetch_check
+    CHECK (last_fetch >= created_at)
 );
 
-GRANT SELECT, INSERT ON applications TO "store-worker";
+CREATE INDEX applications_last_fetch_idx ON applications (last_fetch);
+
+GRANT SELECT, INSERT, UPDATE ON applications TO "store-worker";
