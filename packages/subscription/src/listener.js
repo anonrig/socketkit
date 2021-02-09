16b903e0b5fd6cf72eb50d220d3523bfde0315e8
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-09 09:47:38 +0300

    fix: transaction issues in listeners

--- packages/subscription/src/listener.js
+++ packages/subscription/src/listener.js
@@ -1,11 +1,12 @@
 import { appstoreQueue } from './redis.js'
 import onProcessDate from './consumers/integration/on-process-date.js'
 import Logger from './logger.js'
+import pg from './pg.js'
 
 const logger = Logger.create().withScope('listeners')
 export default async function listenEvents() {
   appstoreQueue.process('process-date', 10, async (job) => {
     logger.success(`Received event=process-date`)
-    await onProcessDate(job.data)
+    await pg.transaction(async (trx) => await onProcessDate(job.data, trx))
   })
 }
