fbc7c7ca252306e55d1634785ec9fdeb091dee75
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-11 16:20:30 +0300

    fix: update dependencies

--- services/marketing/next.config.js
+++ services/marketing/next.config.js
@@ -3,5 +3,9 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['cdn.socketkit.com', 'fra1.digitaloceanspaces.com']
-  }
+  },
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US',
+  },
 }
