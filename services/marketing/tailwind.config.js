106c0122cfd8afe6eca46e0135cd6d95ab8961fa
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-12 14:34:20 +0300

    fix: disable purge

--- services/marketing/tailwind.config.js
+++ services/marketing/tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  // purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
