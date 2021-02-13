fd3fa6a56dea0a40241bfa9058ecc65aa3ed224e
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-13 12:28:49 +0300

    fix: implement application packages through subscriptions table

--- packages/subscription/src/consumers/subscription/index.js
+++ packages/subscription/src/consumers/subscription/index.js
@@ -1,13 +1,26 @@
 import * as Packages from './packages.js'
 
 export const findPackagesGroupByApplication = async (
-  {
-    request: { account_id },
-  },
+  { request: { account_id } },
   callback,
 ) => {
   try {
-    callback(null, { rows: await Packages.findGroupByApplication({ account_id }) } )
+    callback(null, {
+      rows: await Packages.findGroupByApplication({ account_id }),
+    })
+  } catch (error) {
+    callback(error)
+  }
+}
+
+export const findPackages = async (
+  { request: { account_id, application_id } },
+  callback,
+) => {
+  try {
+    callback(null, {
+      rows: await Packages.findAll({ account_id, application_id }, { limit: 10 }),
+    })
   } catch (error) {
     callback(error)
   }
--- packages/subscription/src/consumers/subscription/packages.js
+++ packages/subscription/src/consumers/subscription/packages.js
@@ -10,3 +10,28 @@ export function findGroupByApplication({ account_id }) {
     .where({ account_id })
     .groupBy('application_id')
 }
+
+export async function findAll({ account_id, application_id }, { limit = 10 }) {
+  const rows = await pg
+    .queryBuilder()
+    .select({
+      subscription_name: 'subscription_packages.name',
+      subscription_duration: 'subscription_packages.subscription_duration',
+      subscription_package_id: 'subscription_packages.subscription_package_id',
+      subscription_group_id: 'subscription_packages.subscription_group_id',
+    })
+    .from('subscription_packages')
+    .where({ account_id })
+    .where(function () {
+      if (application_id?.length) {
+        this.andWhere('subscription_packages.application_id', application_id)
+      }
+    })
+    .orderByRaw('subscription_packages.name desc')
+    .limit(limit)
+
+  return rows.map((row) => ({
+    ...row,
+    subscription_duration: row.subscription_duration.toPostgres(),
+  }))
+}
