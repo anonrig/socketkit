6eb9be97152aa56d86e94b7e1f805a56f69475b0
Emre Hasegeli <emre@hasegeli.com>
2021-02-13 12:37:19 +0300

    fix: remove table aliases on subscription-package SQL

--- packages/subscription/src/models/subscription-package.js
+++ packages/subscription/src/models/subscription-package.js
@@ -4,19 +4,19 @@ export async function findAll({ account_id, application_id }, { limit = 10 }) {
   const rows = await pg
     .queryBuilder()
     .select({
-      subscription_name: 'subscription_packages.name',
-      subscription_duration: 'subscription_packages.subscription_duration',
-      subscription_package_id: 'subscription_packages.subscription_package_id',
-      subscription_group_id: 'subscription_packages.subscription_group_id',
+      subscription_name: 'name',
+      subscription_duration: 'subscription_duration',
+      subscription_package_id: 'subscription_package_id',
+      subscription_group_id: 'subscription_group_id',
     })
     .from('subscription_packages')
     .where({ account_id })
     .where(function () {
       if (application_id?.length) {
-        this.andWhere('subscription_packages.application_id', application_id)
+        this.andWhere('application_id', application_id)
       }
     })
-    .orderByRaw('subscription_packages.name desc')
+    .orderByRaw('name desc')
     .limit(limit)
 
   return rows.map((row) => ({
