43c169d81ff1cb1c73b2391e30aaf3a3b82023fe
Emre Hasegeli <emre@hasegeli.com>
2021-02-15 11:16:56 +0300

    fix: add default service URLs

--- packages/core/src/config.js
+++ packages/core/src/config.js
@@ -3,8 +3,8 @@ export default {
   port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
   postgresql: process.env.DATABASE_URL,
   grpc: {
-    subscription: process.env.SUBSCRIPTION_GRPC_URL,
-    store: process.env.STORE_GRPC_URL,
+    subscription: process.env.SUBSCRIPTION_GRPC_URL ?? 'localhost:3001',
+    store: process.env.STORE_GRPC_URL ?? 'localhost:3003',
   },
   redis: {
     host: process.env.REDIS_HOST,
