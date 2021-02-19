687b322c4b89989c2ac0fdb25002abaca725fbcd
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-19 15:22:51 +0300

    fix(build): update package-lock

--- packages/web/pages/reports/index.js
+++ packages/web/pages/reports/index.js
@@ -41,7 +41,7 @@ export default function Reports({ initialData }) {
           axisBottom={{
             tickSize: 5,
             tickPadding: 5,
-            tickRotation: 0
+            tickRotation: 0,
           }}
           labelSkipWidth={12}
           labelSkipHeight={12}
