9794703c305fa83b8b596d95bbf3a9adb4238160
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-23 16:21:33 +0300

    fix: remove unnecessary test

--- packages/subscription/test/integrations.test.js
+++ packages/subscription/test/integrations.test.js
@@ -4,8 +4,6 @@ import logger from '../src/logger.js'
 import app from '../src/grpc.js'
 import { v4 } from 'uuid'
 
-const TEST_ACCOUNT_ID = `58e670db-f4ee-407d-979e-3e0d88c8eeb8`
-
 beforeAll((done) => {
   logger.pauseLogs()
   app.start(`0.0.0.0:${config.port}`)
@@ -28,24 +26,6 @@ describe('Integrations', () => {
           expect(error).toBeNull()
           expect(response).toBeInstanceOf(Object)
           expect(response.state).toBeDefined()
-          done()
-        } catch (error) {
-          done(error)
-        }
-      },
-    )
-  })
-
-  test('findLatestScrape', (done) => {
-    grpc.integrations.findLatestScrape(
-      {
-        account_id: TEST_ACCOUNT_ID,
-      },
-      (error, response) => {
-        try {
-          expect(error).toBeNull()
-          expect(response).toBeInstanceOf(Object)
-          expect(response.row.fetch_date).toBeDefined()
           done()
         } catch (error) {
           done(error)
