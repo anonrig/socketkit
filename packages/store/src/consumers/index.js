1bfe5ce1f7ed827db4b40b65d2425d341316ace5
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-23 23:25:06 +0300

    fix: properly handle 404 errors from scraper

--- packages/store/src/consumers/index.js
+++ packages/store/src/consumers/index.js
@@ -2,6 +2,7 @@ import * as Applications from '../models/application.js'
 import * as Reviews from '../models/reviews.js'
 import pg from '../pg.js'
 import scraper from 'app-store-scraper'
+import logger from '../logger.js'
 
 export async function findAll(ctx) {
   const { application_ids, bundle_ids, developer_ids } = ctx.req
@@ -39,11 +40,23 @@ export async function create(ctx) {
   if (await Applications.findOne({ application_id })) {
     ctx.res = { row: {} }
   } else {
-    const scraped_app = await scraper.app({ id: application_id, ratings: true })
-    ctx.res = {
-      row: await pg.transaction((trx) =>
-        Applications.create(scraped_app, country_id, trx),
-      ),
+    try {
+      const scraped_app = await scraper.app({
+        id: application_id,
+        ratings: true,
+      })
+      ctx.res = {
+        row: await pg.transaction((trx) =>
+          Applications.create(scraped_app, country_id, trx),
+        ),
+      }
+    } catch (error) {
+      if (error.message.includes('404')) {
+        ctx.res = { row: null }
+      } else {
+        logger.error(error)
+        throw error
+      }
     }
   }
 }
