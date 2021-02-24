06aa58db11f9486b91ec4d7194ef1ac18cab7eee
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-24 09:51:28 +0300

    fix: kratos endpoints

--- packages/web/helpers/kratos.js
+++ packages/web/helpers/kratos.js
@@ -1,9 +1,9 @@
 export const currentPath = process.env.NEXT_PUBLIC_CURRENT_URL
 export const basePath = process.env.NEXT_PUBLIC_KRATOS_URL
 export const endpoints = {
-  login: `${basePath}/self-service/login/browser?return_to=/`,
-  register: `${basePath}/self-service/registration/browser?return_to=/`,
-  recover: `${basePath}/self-service/recovery/browser?return_to=/`,
-  profile: `${basePath}/self-service/settings/browser?return_to=/settings`,
-  logout: `${basePath}/self-service/browser/flows/logout?return_to=/`,
+  login: `${basePath}/self-service/login/browser`,
+  register: `${basePath}/self-service/registration/browser`,
+  recover: `${basePath}/self-service/recovery/browser`,
+  profile: `${basePath}/self-service/settings/browsersettings`,
+  logout: `${basePath}/self-service/browser/flows/logout`,
 }
