10194ebea4376ac5f5e3c49e46f93ea40792fdd4
Yagiz Nizipli <yagiz@nizipli.com>
2021-02-27 12:21:58 +0300

    fix: ratings issue if no ratings are available

--- packages/web/helpers/index.js
+++ packages/web/helpers/index.js
@@ -1,5 +1,5 @@
 export const averageRating = (ratings) => {
-  if (!ratings) return null
+  if (!ratings || !ratings.length) return null
   const sum = ratings?.map((r, i) => r * (i + 1)).reduce((a, b) => a + b)
   const count = ratings?.reduce((a, b) => a + b)
   return (sum / count).toFixed(2)
