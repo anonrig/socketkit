42b0da7fcbd7b5d816707b6de11ee599ee971440
Emre Hasegeli <emre@hasegeli.com>
2021-03-10 10:56:08 +0300

    feat: add subscriber report

--- packages/core/src/routes/reports/index.js
+++ packages/core/src/routes/reports/index.js
@@ -1,10 +1,12 @@
 import trials from './trials.js'
+import subscribers from './subscribers.js'
 import mrr from './mrr.js'
 import averageDuration from './average-duration.js'
 
 export default (f, _opts, done) => {
   f.route(averageDuration)
   f.route(trials)
+  f.route(subscribers)
   f.route(mrr)
   done()
 }
---
+++ packages/core/src/routes/reports/subscribers.js
@@ -0,0 +1,63 @@
+import { verify } from '../../hooks.js'
+import f from '../../server.js'
+
+export default {
+  method: 'GET',
+  path: '/subscribers',
+  schema: {
+    querystring: {
+      type: 'object',
+      properties: {
+        start_date: {
+          type: 'string',
+          format: 'date',
+        },
+        end_date: {
+          type: 'string',
+          format: 'date',
+        },
+        interval: {
+          type: 'string',
+          default: 'week',
+        },
+      },
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          available_filters: {
+            type: 'array',
+            items: { type: 'string' },
+          },
+          rows: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                primary: { type: 'string' },
+                secondary: { type: 'number' },
+                previous_secondary: { type: 'number' },
+              },
+            },
+          },
+          secondary_field: { type: 'string' },
+          fields: { type: 'array', items: { type: 'string' } },
+        },
+      },
+    },
+  },
+  preHandler: verify,
+  handler: async ({ accounts: [account], query }) => {
+    if (!account) {
+      throw f.httpErrors.notFound(`Account not found`)
+    }
+
+    return f.grpc.reports.subscribers({
+      account_id: account.account_id,
+      start_date: query.start_date,
+      end_date: query.end_date,
+      interval: `1 ${query.interval}`,
+    })
+  },
+}
