4fea68c6222bfc96fe9bc754df91bf929e0eb9c8
Emre Hasegeli <emre@hasegeli.com>
2021-03-11 18:44:38 +0300

    fix: rename subscriber reports as subscription

--- packages/subscription/src/consumers/reports/index.js
+++ packages/subscription/src/consumers/reports/index.js
@@ -1,13 +1,13 @@
 import * as TrialReports from '../../models/trial-reports.js'
-import * as SubscriberReports from '../../models/subscriber-reports.js'
+import * as SubscriptionReports from '../../models/subscription-reports.js'
 import * as RevenueReports from '../../models/revenue-reports.js'
 
 export const trials = async (ctx) => {
   ctx.res = await TrialReports.getFreeTrials(ctx.req)
 }
 
-export const subscribers = async (ctx) => {
-  ctx.res = await SubscriberReports.get(ctx.req)
+export const subscriptions = async (ctx) => {
+  ctx.res = await SubscriptionReports.get(ctx.req)
 }
 
 export const averageDuration = async (ctx) => {
