1e88ba052f041d42d2879a8e9e67d7fc822bd7ac
Emre Hasegeli <emre@hasegeli.com>
2021-03-14 19:44:29 +0300

    refactor: generalize reports

--- packages/core/src/routes/reports.js
+++ packages/core/src/routes/reports.js
@@ -2,42 +2,9 @@ import { verify } from '../hooks.js'
 import f from '../server.js'
 
 export default (f, _opts, done) => {
-  addRoute(f, '/average-duration', f.grpc.reports.averageDuration, {
-    average_trial_duration: { type: 'number' },
-    average_subscription_duration: { type: 'number' },
-  })
-  addRoute(f, '/subscribers', f.grpc.reports.subscribers, {
-    count: { type: 'number' },
-    avg_age: { type: 'string' },
-  })
-  addRoute(f, '/trials', f.grpc.reports.trials, {
-    secondary: { type: 'number' },
-    previous_secondary: { type: 'number' },
-  })
-  addRoute(f, '/subscriptions', f.grpc.reports.subscriptions, {
-    count: { type: 'number' },
-    avg_total_base_developer_proceeds: { type: 'string' },
-  })
-  addRoute(f, '/mrr', f.grpc.reports.mrr, {
-    month: { type: 'string' },
-    mrr: { type: 'string' },
-    clients: { type: 'number' },
-    new_mrr: { type: 'string' },
-    expansion_mrr: { type: 'string' },
-    churned_mrr: { type: 'string' },
-    contraction_mrr: { type: 'string' },
-    net_new_mrr: { type: 'string' },
-    mrr_churn: { type: 'string' },
-    clients_churn: { type: 'number' },
-    arpu: { type: 'string' },
-  })
-  done()
-}
-
-function addRoute(f, path, grpc_method, additional_properties) {
   f.route({
     method: 'GET',
-    path: path,
+    path: '/subscription/:report_id',
     schema: {
       querystring: {
         type: 'object',
@@ -66,10 +33,19 @@ function addRoute(f, path, grpc_method, additional_properties) {
               items: {
                 type: 'object',
                 properties: {
-                  primary: { type: 'string' },
-                  ...additional_properties,
+                  x: { type: 'string' },
+                  y0: { type: 'number' },
+                  y1: { type: 'number' },
+                  y2: { type: 'number' },
+                  y3: { type: 'number' },
+                  y4: { type: 'number' },
+                  y5: { type: 'number' },
+                  y6: { type: 'number' },
+                  y7: { type: 'number' },
+                  y8: { type: 'number' },
+                  y9: { type: 'number' },
                 },
-                required: ['primary'],
+                required: ['x', 'y0'],
               },
             },
           },
@@ -77,12 +53,13 @@ function addRoute(f, path, grpc_method, additional_properties) {
       },
     },
     preHandler: verify,
-    handler: async ({ accounts: [account], query }) => {
+    handler: async ({ accounts: [account], query, params }) => {
       if (!account) {
         throw f.httpErrors.notFound(`Account not found`)
       }
 
-      return grpc_method({
+      return f.grpc.reports.get({
+        report_id: params.report_id,
         account_id: account.account_id,
         start_date: query.start_date,
         end_date: query.end_date,
@@ -90,4 +67,5 @@ function addRoute(f, path, grpc_method, additional_properties) {
       })
     },
   })
+  done()
 }
