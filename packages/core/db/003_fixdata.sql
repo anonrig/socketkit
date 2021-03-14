610040997913d6ec46481cd6bf61905299c6a84a
Emre Hasegeli <emre@hasegeli.com>
2021-03-14 14:20:57 +0300

    fix: remove obsolete database tables

--- packages/core/db/003_fixdata.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO integrations (integration_id, title, description, requirement_schema)
-    VALUES (
-        'appstore-connect',
-        'AppStore Connect',
-        'Transactions and Sales support for Apple',
-        '{"type": "object", "required": ["access_token"], "properties": {"access_token": {"type": "string"}}}'
-    );
--- packages/core/src/methods/accounts.js
+++ packages/core/src/methods/accounts.js
@@ -4,24 +4,14 @@ import { v4 } from 'uuid'
 export async function getAccounts({ identity_id }) {
   return pg
     .queryBuilder()
-    .select({ account_id: 'account_identities.account_id' })
+    .select('account_id')
     .from('account_identities')
-    .innerJoin('accounts', function () {
-      this.on('account_identities.account_id', 'accounts.account_id')
-    })
-    .where(function () {
-      this.where('account_identities.identity_id', identity_id)
-    })
+    .where('identity_id', identity_id)
 }
 
 export async function createAccount({ identity_id }) {
   return pg.transaction(async (trx) => {
     const account_id = v4()
-
-    await trx('accounts')
-      .insert({ account_id })
-      .returning('account_id')
-      .transacting(trx)
 
     return trx('account_identities')
       .insert({
