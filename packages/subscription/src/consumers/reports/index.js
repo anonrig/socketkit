5ad8ba2993d72a880fee0b2f10364ed3a036deb5
Yagiz Nizipli <yagiz@nizipli.com>
2021-03-16 14:24:48 +0300

    refactor: use map instead of object for mapping reports

--- packages/subscription/src/consumers/reports/index.js
+++ packages/subscription/src/consumers/reports/index.js
@@ -3,21 +3,21 @@ import * as TrialReports from '../../models/trial-reports.js'
 import * as SubscriptionReports from '../../models/subscription-reports.js'
 import * as RevenueReports from '../../models/revenue-reports.js'
 
-const reports = {
-  subscribers: SubscriberReports.get,
-  trials: TrialReports.getFreeTrials,
-  'average-sales-cycle': TrialReports.averageDuration,
-  subscriptions: SubscriptionReports.get,
-  'average-revenue-per-subscription': SubscriptionReports.get,
-  mrr: RevenueReports.getMRR,
-  'sales-refunds': RevenueReports.getSalesRefunds,
-  'average-sale': RevenueReports.getAverageSale,
-}
+const reports = new Map([
+  ['subscribers', SubscriberReports.get],
+  ['trials', TrialReports.getFreeTrials],
+  ['average-sales-cycle', TrialReports.averageDuration],
+  ['subscriptions', SubscriberReports.get],
+  ['average-revenue-per-subscription', SubscriptionReports.get],
+  ['mrr', RevenueReports.getMRR],
+  ['sales-refunds', RevenueReports.getSalesRefunds],
+  ['average-sale', RevenueReports.getAverageSale],
+])
 
 export const get = async (ctx) => {
-  const report_func = reports[ctx.req.report_id]
-  if (!report_func) {
-    throw new Error('Report not found')
+  const { report_id } = ctx.req
+  if (!reports.has(report_id)) {
+    throw new Error(`Report not found`)
   }
-  ctx.res = await report_func(ctx.req)
+  ctx.res = await reports.get(report_id)(ctx.req)
 }
