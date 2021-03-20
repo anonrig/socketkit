423cad2095994019a83d39d8161d131a3afdb5fb
Yagiz Nizipli <yagiz@nizipli.com>
2021-03-20 20:56:10 +0300

    fix: handle login errors due to invalid transaction

--- packages/core/src/models/accounts.js
+++ packages/core/src/models/accounts.js
@@ -10,14 +10,26 @@ export async function getAccounts({ identity_id }) {
 }
 
 export async function createAccount({ identity_id }) {
-  return pg
-    .queryBuilder()
-    .insert({
-      account_role: 'owner',
-      created_at: new Date(),
-      account_id: v4(),
-      identity_id,
-    })
-    .into('account_identities')
-    .returning('*')
+  return pg.transaction(async (trx) => {
+    const account_id = v4()
+
+    await pg
+      .queryBuilder()
+      .insert({ account_id })
+      .into('accounts')
+      .transacting(trx)
+      .returning('account_id')
+
+    return pg
+      .queryBuilder()
+      .insert({
+        account_role: 'owner',
+        created_at: new Date(),
+        account_id,
+        identity_id,
+      })
+      .into('account_identities')
+      .returning('*')
+      .transacting(trx)
+  })
 }
