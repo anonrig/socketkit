a6b8c4e894ad3998c9c49830342cf4fdcb3cb15a
Yagiz Nizipli <yagiz@nizipli.com>
2021-04-01 09:00:36 +0300

    feat: change range picker to date picker on dashboard

--- packages/web/pages/index.js
+++ packages/web/pages/index.js
@@ -1,11 +1,11 @@
 import { useContext, useState } from 'react'
 import dayjs from 'dayjs'
 
+import DatePicker from 'components/date-picker.js'
+import { AuthContext } from 'helpers/is-authorized.js'
 import { fetcher } from 'helpers/fetcher.js'
 import CountriesWidget from 'components/scenes/dashboard/countries-widget.js'
-import RangePicker from 'components/scenes/dashboard/range-picker.js'
 import StatisticsWidget from 'components/scenes/dashboard/statistics-widget.js'
-import { AuthContext } from 'helpers/is-authorized.js'
 import CustomersWidget from '../components/scenes/dashboard/subscribers-widget.js'
 
 export async function getServerSideProps({
@@ -33,27 +33,10 @@ export async function getServerSideProps({
 
 export default function Dashboard({ countries }) {
   const { session } = useContext(AuthContext)
-  const ranges = [
-    {
-      key: 'last-90',
-      title: 'Last 90 Days',
-      from: dayjs().subtract(3, 'month').format('YYYY-MM-DD'),
-      to: dayjs().format('YYYY-MM-DD'),
-    },
-    {
-      key: 'last-30',
-      title: 'Last 30 Days',
-      from: dayjs().subtract(1, 'month').format('YYYY-MM-DD'),
-      to: dayjs().format('YYYY-MM-DD'),
-    },
-    {
-      key: 'last-7',
-      title: 'Last 7 Days',
-      from: dayjs().subtract(7, 'days').format('YYYY-MM-DD'),
-      to: dayjs().format('YYYY-MM-DD'),
-    },
-  ]
-  const [selected, setSelected] = useState(ranges[1])
+  const [interval, setInterval] = useState({
+    from: dayjs().subtract(1, 'month'),
+    to: dayjs(),
+  })
 
   const getLabel = () => {
     const hr = dayjs().hour()
@@ -75,12 +58,24 @@ export default function Dashboard({ countries }) {
           </h3>
         </div>
 
-        <RangePicker selected={selected} setSelected={setSelected} ranges={ranges} />
+        <DatePicker
+          interval={{ start_date: interval.from, end_date: interval.to }}
+          setInterval={({ start_date, end_date }) =>
+            setInterval({ from: start_date, to: end_date })
+          }
+        />
       </div>
       <section className="space-y-10">
-        <StatisticsWidget range={selected} />
-        <CustomersWidget range={selected} />
-        <CountriesWidget range={selected} initialData={countries} />
+        <StatisticsWidget
+          range={{ from: interval.from.format('YYYY-MM-DD'), to: interval.to.format('YYYY-MM-DD') }}
+        />
+        <CustomersWidget
+          range={{ from: interval.from.format('YYYY-MM-DD'), to: interval.to.format('YYYY-MM-DD') }}
+        />
+        <CountriesWidget
+          range={{ from: interval.from.format('YYYY-MM-DD'), to: interval.to.format('YYYY-MM-DD') }}
+          initialData={countries}
+        />
       </section>
     </>
   )
