360718f18fc26517a99d59089911b0a74b0ea658
Yagiz Nizipli <yagiz@nizipli.com>
2021-04-02 17:45:41 +0300

    style: update reviews page design

--- packages/web/pages/applications/[id]/reviews.js
+++ packages/web/pages/applications/[id]/reviews.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
 import { useMemo } from 'react'
 import PropTypes from 'prop-types'
+
 import Table from 'components/table/table'
 import { fetcher } from 'helpers/fetcher'
 import InlineRating from 'components/inline-rating.js'
@@ -39,13 +40,13 @@ function ApplicationReviews({ initialData, id }) {
         accessor: function CountryAccessor(fields) {
           const country_id = fields.country_id.toUpperCase()
           return (
-            <div className="flex flex-row">
-              {fields.country_id.toUpperCase()}
-              <InlineRating rating={fields.score ?? 1} className="ml-2"/>
+            <div className="flex flex-row space-x-2 flex-1">
+              {country_id}
+              <InlineRating rating={fields.score ?? 1} className="ml-1"/>
             </div>
           )
         },
-        className: 'text-left font-semibold',
+        className: 'text-left font-semibold w-24',
       },
       {
         Header: 'Version',
@@ -55,9 +56,14 @@ function ApplicationReviews({ initialData, id }) {
       {
         Header: 'Content',
         accessor: function ContentAccessor(field) {
-          return <div className="w-92 overflow-ellipsis">{field.content}</div>
+          return (
+            <div className="space-y-1 w-full relative overflow-hidden">
+              <div className="text-xs font-semibold">{field.title} - <a className="font-bold hover:text-orange-400" href={field.user_url}>{field.username}</a></div>
+              <p className="text-sm line-clamp-2">{field.content}</p>
+            </div>
+          )
         },
-        className: 'text-left text-sm overflow-ellipsis',
+        className: 'text-left',
       },
     ],
     [],
