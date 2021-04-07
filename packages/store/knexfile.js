442a16a405a976d68cdd1cdbf939f8a68193ae9c
Yagiz Nizipli <yagiz@nizipli.com>
2021-04-07 09:56:11 +0300

    fix: remove unnecessary path

--- packages/store/knexfile.js
+++ packages/store/knexfile.js
@@ -1,4 +1,3 @@
-import path from 'path'
 import Knex from 'knex'
 
 /**
@@ -13,13 +12,6 @@ const config = {
     port: 5432,
   },
   pool: { min: 0, max: 5 },
-  migrations: {
-    tableName: 'migrations',
-    directory: path.join(path.resolve(), 'db/migrations'),
-  },
-  seeds: {
-    directory: path.join(path.resolve(), 'db/seeds'),
-  },
   searchPath: ['public'],
   useNullAsDefault: false,
   asyncStackTraces: true,
