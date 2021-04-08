daf88552cf0a54d99f4efffeaf584e2b60569714
Yagiz Nizipli <yagiz@nizipli.com>
2021-04-08 11:58:29 +0300

    fix: remove unnecessary modules

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,18 +1,14 @@
 const withPlugins = require('next-compose-plugins')
-const withPreact = require('next-plugin-preact')
 const withImages = require('next-images')
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 
-module.exports = withPlugins([withPreact, withBundleAnalyzer, withImages], {
+module.exports = withPlugins([withImages], {
   poweredByHeader: false,
   reactStrictMode: true,
   env: {
     NEXT_PUBLIC_KRATOS_URL: 'https://login.socketkit.com',
     NEXT_PUBLIC_API_URL: 'https://core.socketkit.com/v1',
   },
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /react-spring/,
       sideEffects: true,
