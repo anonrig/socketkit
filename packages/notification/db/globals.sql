04f6ae3bd7239fc64425de7a46a96b56eb5ea360
Emre Hasegeli <emre@hasegeli.com>
2021-04-12 09:01:03 +0300

    Remove db password from globals.sql

--- packages/notification/db/globals.sql
+++ packages/notification/db/globals.sql
@@ -1,4 +1,5 @@
 CREATE ROLE notification;
-CREATE ROLE "notification-worker" LOGIN PASSWORD 'EC3Vov_e38qWMRXC';
+CREATE ROLE "notification-worker" LOGIN PASSWORD 'change-this';
 
-CREATE DATABASE notification OWNER notification TEMPLATE template0 ENCODING "UTF-8" LC_COLLATE "C" LC_CTYPE "C";
+CREATE DATABASE notification OWNER notification
+TEMPLATE template0 ENCODING "UTF-8" LC_COLLATE "C" LC_CTYPE "C";
