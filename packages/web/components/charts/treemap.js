53ea409f8d43bdb1b4001603eaa99e9d11a033b7
Yagiz Nizipli <yagiz@nizipli.com>
2021-04-21 16:38:09 +0300

    fix: remove unnecessary treemap prop-types

--- packages/web/components/charts/treemap.js
+++ packages/web/components/charts/treemap.js
@@ -37,12 +37,10 @@ function TreeMapChart({ id, rows, identity }) {
 }
 
 TreeMapChart.propTypes = {
-  id: PropTypes.string.isRequired,
   identity: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   rows: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string.isRequired,
       value: PropTypes.number.isRequired,
       color: PropTypes.string.isRequired,
     }),
