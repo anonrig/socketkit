6cb0e85c8a554a33f0aa30970e7bf2a7c70425d8
Yagiz Nizipli <yagiz@nizipli.com>
2021-04-21 17:05:20 +0300

    fix: handle config for nextjs

--- packages/web/helpers/config.js
+++ packages/web/helpers/config.js
@@ -1,7 +1,7 @@
-export const kratosUrl = process.env.NEXT_PUBLIC_KRATOS_URL ?? 'https://login.socketkit.com'
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? 'https://core.socketkit.com/v1'
-export const currentUrl = process.env.NEXT_PUBLIC_CURRENT_URL ?? 'https://web.socketkit.com'
+export const kratosUrl = process.env.KRATOS_URL ?? 'https://login.socketkit.com'
+export const apiUrl = process.env.API_URL ?? 'https://core.socketkit.com/v1'
+export const currentUrl = process.env.CURRENT_URL ?? 'https://web.socketkit.com'
 
 export const stripeKey =
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ??
+  process.env.STRIPE_PUBLISHABLE_KEY ??
   'pk_test_51I1bf6EArFRUZZMzlWlSMAufFf0Fi4rdZEMqcYzdtcBAdZEMbZplUjq6tvA2jFC92boylKm1xPE4v3BXMCdB7Io7003UraELJK'
--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -8,6 +8,12 @@ module.exports = {
     })
     return config
   },
+  env: {
+    KRATOS_URL: process.env.KRATOS_URL,
+    API_URL: process.env.API_URL,
+    CURRENT_URL: process.env.CURRENT_URL,
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+  },
   async redirects() {
     return [
       {
