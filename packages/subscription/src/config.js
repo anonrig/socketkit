f610b5629af0ddb6b5b11152b4b37be97731f436
Emre Hasegeli <emre@hasegeli.com>
2021-04-21 17:16:21 +0300

    Get rid of knexfile.js

--- packages/subscription/src/config.js
+++ packages/subscription/src/config.js
@@ -1,4 +1,13 @@
 export default {
   port: process.env.PORT ? parseInt(process.env.PORT) : 3001,
   fixerKey: process.env.FIXER_KEY,
+  knex: {
+    client: 'pg',
+    version: '13',
+    connection: {
+      database: 'subscription',
+      user: 'subscription-worker',
+    },
+    pool: { min: 2, max: 10 },
+  },
 }
--- packages/subscription/src/pg.js
+++ packages/subscription/src/pg.js
@@ -1,9 +1,9 @@
 import knex from 'knex'
-import config from '../knexfile.js'
+import config from './config.js'
 import pg from 'pg'
 import range from 'pg-range'
 
 pg.types.setTypeParser(20, 'text', parseInt)
 range.install(pg)
 
-export default knex(config)
+export default knex(config.knex)
