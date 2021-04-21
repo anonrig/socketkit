0ad516923acaabaeada71d3e09c5aa4bb5121d19
Emre Hasegeli <emre@hasegeli.com>
2021-04-21 17:21:09 +0300

    Get rid of knexfile.js

--- packages/notification/src/config.js
+++ packages/notification/src/config.js
@@ -1,4 +1,13 @@
 export default {
   isProduction: process.env.NODE_ENV === 'production',
   port: process.env.PORT ? parseInt(process.env.PORT) : 3004,
+  knex: {
+    client: 'pg',
+    version: '13',
+    connection: {
+      database: 'notification',
+      user: 'notification-worker',
+    },
+    pool: { min: 0, max: 5 },
+  },
 }
