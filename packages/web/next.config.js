f2ddc2f73f18baeb655e27be3492d7e9ecfc74b4
Yagiz Nizipli <yagiz@nizipli.com>
2021-05-02 18:07:42 +0300

    build: remove custom webpack configuration

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /react-spring/,
-      sideEffects: true,
-    })
-    return config
-  },
   env: {
     KRATOS_URL: process.env.KRATOS_URL,
     API_URL: process.env.API_URL,
@@ -23,5 +16,4 @@ module.exports = {
       },
     ]
   },
-  future: { webpack5: true, strictPostcssConfiguration: true },
 }
