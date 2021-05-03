d6ace3d2548ba94b1f4e30980f697cd4a1ce1fb4
Yagiz Nizipli <yagiz@nizipli.com>
2021-05-03 10:02:27 +0300

    feat: handle discord input validation

--- packages/notification/src/providers/discord.js
+++ packages/notification/src/providers/discord.js
@@ -1,5 +1,9 @@
 import { Webhook, MessageBuilder } from 'discord-webhook-node'
 import dayjs from 'dayjs'
+import grpc from '@grpc/grpc-js'
+
+import validator from '../validator.js'
+import * as Schemas from './discord.schema.js'
 import {
   countryCodeEmoji,
   getRatingEmojis,
@@ -8,44 +12,57 @@ import {
 
 // https://github.com/matthew1232/discord-webhook-node
 export async function send(type = 'review', url, properties) {
+  const schema = Schemas[type]
+
+  if (!schema) {
+    const error = new Error(
+      `Type of ${type} is not available for Slack integration`,
+    )
+    error.code = grpc.status.NOT_FOUND
+    throw error
+  }
+  const validated_properties = validator.validate(
+    Schemas[type],
+    convertPropertiesObject(properties),
+  )
+
+  if (!validated_properties) {
+    const error = new Error(validator.errors[0].message)
+    error.code = grpc.status.FAILED_PRECONDITION
+    throw error
+  }
+
   if (type === 'review') {
-    sendReview(url, properties)
+    sendReview(url, validated_properties)
   }
 }
 
 export async function sendReview(url, properties) {
-  const {
-    username,
-    title,
-    content,
-    country_id,
-    score,
-    sent_at,
-    application_id,
-    application_title,
-    application_icon,
-  } = convertPropertiesObject(properties)
   const hook = new Webhook(url)
   const embed = new MessageBuilder()
-    .setTitle(title)
+    .setTitle(properties.title)
     .setAuthor(
       'Socketkit',
       'https://cdn.socketkit.com/assets/icon.png',
       'https://socketkit.com',
     )
-    .setThumbnail(application_icon)
-    .setURL(`https://web.socketkit.com/applications/${application_id}/reviews`)
-    .setDescription(content)
-    .addField('Application', application_title)
-    .addField('Username', username)
-    .addField('Rating', getRatingEmojis(score))
+    .setThumbnail(properties.application_icon)
+    .setURL(properties.review_url)
+    .setDescription(properties.content)
+    .addField('Application', properties.application_title)
+    .addField('Username', properties.username)
+    .addField('Rating', getRatingEmojis(properties.score))
     .addField(
       'Country',
-      `${country_id.toUpperCase()} ${countryCodeEmoji(
-        country_id.toUpperCase(),
+      `${properties.country_id.toUpperCase()} ${countryCodeEmoji(
+        properties.country_id.toUpperCase(),
       )}`,
     )
-    .addField(`Reviewed at`, dayjs(sent_at).format('DD.MM.YYYY HH:MM'), true)
+    .addField(
+      `Reviewed at`,
+      dayjs(properties.sent_at).format('DD.MM.YYYY HH:MM'),
+      true,
+    )
     .setFooter('Access this review from https://socketkit.com')
     .setTimestamp()
 
