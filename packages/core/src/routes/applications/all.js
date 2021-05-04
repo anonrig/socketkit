d442da6f78b1743adf2dd19f8ed790dd95d91085
Emre Hasegeli <emre@hasegeli.com>
2021-05-04 09:38:46 +0300

    Handle missing applications on store

--- packages/core/src/routes/applications/all.js
+++ packages/core/src/routes/applications/all.js
@@ -27,22 +27,22 @@ export default {
   },
   preHandler: verify,
   handler: async ({ accounts: [account] }) => {
-    const applications = await grpc.subscriptions.groupByApplication({
+    const { rows } = await grpc.subscriptions.groupByApplication({
       account_id: account.account_id,
     })
 
-    const application_ids = applications.rows.map((app) => app.application_id)
-    const { rows: storedApplications } = await grpc.applications.findAll({
+    const application_ids = rows.map((r) => r.application_id)
+    const { rows: applications } = await grpc.applications.findAll({
       application_ids,
     })
 
-    return Object.assign({}, applications, {
-      rows: applications.rows.map((application) => ({
+    return {
+      rows: applications.map((application) => ({
         ...application,
-        ...(storedApplications.filter(
-          (a) => a.application_id === application.application_id,
+        ...(rows.filter(
+          (r) => r.application_id === application.application_id,
         )[0] ?? {}),
       })),
-    })
+    }
   },
 }
