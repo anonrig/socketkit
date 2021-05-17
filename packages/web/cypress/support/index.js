74a155dbdc3dadda15c63458147f071adf23835a
Yagiz Nizipli <yagiz@nizipli.com>
2021-05-17 22:04:06 +0300

    feat: add test-coverage calculation to nextjs

--- packages/web/cypress/support/index.js
+++ packages/web/cypress/support/index.js
@@ -13,6 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
+import '@cypress/code-coverage/support'
 import './commands'
 
 Cypress.on('uncaught:exception', () => {
