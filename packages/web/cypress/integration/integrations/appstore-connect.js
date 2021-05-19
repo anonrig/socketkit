567942edee4d6fd3d43549273804663add86613b
Yagiz Nizipli <yagiz@nizipli.com>
2021-05-19 12:37:35 +0300

    test: fix appstore-connect integration tests

--- packages/web/cypress/integration/integrations/appstore-connect.js
+++ packages/web/cypress/integration/integrations/appstore-connect.js
@@ -19,12 +19,12 @@ context('Settings > Integrations', () => {
     cy.get('button[type="submit"]').click()
     cy.get('.pointer-events-auto').should('contain', 'Token validation failed')
     cy.get('.space-x-4 :nth-child(1)').click()
-    cy.location('pathname').should('have.value', '/account/integrations')
+    cy.location().should((loc) => expect(loc.pathname).to.eq('/account/integrations'))
   })
 
   it('should go back to integrations on cancel', () => {
     cy.visit('/account/integrations/appstore-connect')
     cy.get('.space-x-4 :nth-child(1)').click()
-    cy.location('pathname').should('have.value', '/account/integrations')
+    cy.location().should((loc) => expect(loc.pathname).to.eq('/account/integrations'))
   })
 })
