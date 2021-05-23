ca79c2fb550a74644260a34f7d4f4831bb582af2
Yagiz Nizipli <yagiz@nizipli.com>
2021-05-23 16:08:18 +0300

    test: fix transaction tests

--- packages/subscription/test/transaction.test.js
+++ packages/subscription/test/transaction.test.js
@@ -22,35 +22,40 @@ const STANDARD = {
   developerProceeds: '9.99',
   customerCurrency: 'USD',
   proceedsCurrency: 'USD',
-  standardSubscriptionDuration: '1 Month',
+  standardSubscriptionDuration: '1 Week',
+  eventDate: dayjs().subtract(1, 'week').format('YYYY-MM-DD'),
+}
+
+const exchange_rates = {
+  USD: 1,
 }
 
 describe('Transaction', () => {
   test('should parse free trial correctly', async (done) => {
-    const t = new Transaction(WITH_FREE_TRIAL)
-    const rates = await t.getExchangeRates()
+    const t = new Transaction(WITH_FREE_TRIAL, exchange_rates)
     expect(t.type).toEqual('trial')
-    expect(rates).toEqual([{ amount: 0 }, { amount: 0 }])
     expect(t.subscription_refunded_at).toBeFalsy()
     expect(t.duration).toBeTruthy()
     done()
   })
 
   test('should parse refund correctly', (done) => {
-    const t = new Transaction(REFUND)
+    const t = new Transaction(REFUND, exchange_rates)
     expect(t.type).toEqual('refund')
     expect(t.subscriber_purchase).toEqual(t.developer_proceeds)
-    expect(t.subscription_refunded_at).toEqual(REFUND.purchaseDate)
+    expect(t.purchase_date?.format('YYYY-MM-DD')).toEqual(REFUND.purchaseDate)
     done()
   })
 
   test('should parse renewal correctly', async (done) => {
-    const t = new Transaction(STANDARD)
-    await t.getExchangeRates()
+    const t = new Transaction(STANDARD, exchange_rates)
     t.total_base_developer_proceeds = '9.99'
     expect(t.type).toEqual('renewal')
     expect(t.subscription_refunded_at).toBeFalsy()
     expect(t.duration).toBeTruthy()
+    expect(t.subscription_expired_at.format('YYYY-MM-DD')).toEqual(
+      dayjs().format('YYYY-MM-DD'),
+    )
     done()
   })
 })
