3d5a10510c8d8ff06e55246acfe01c36bf0c92d5
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-02 12:25:35 +0300

    fix: fix a bug with appstore integration validation

--- packages/subscription/src/consumers/integration/on-validate.js
+++ packages/subscription/src/consumers/integration/on-validate.js
@@ -4,7 +4,7 @@ import logger from '../../logger.js'
 export default async function onValidate(accessToken) {
   try {
     const reporter = new AppStoreReporter.default({ accessToken })
-    const response = await reporter.default.sales.getStatus()
+    const response = await reporter.sales.getStatus()
     return response
   } catch (error) {
     logger.withScope('integrations').withTag('onValidate').warn(error)
