c6106c0c8898dcc46ab4ded948fc3bdf929bfd59
Yagiz Nizipli <yagiz@nizipli.com>
2021-06-02 19:08:57 +0300

    fix: remove users endpoint

--- packages/core/src/routes/accounts/index.js
+++ packages/core/src/routes/accounts/index.js
@@ -1,10 +1,8 @@
 import statistics from './statistics.js'
 import countries from './countries.js'
-import users from './users.js'
 
 export default (f, _opts, done) => {
   f.route(statistics)
   f.route(countries)
-  f.route(users)
   done()
 }
