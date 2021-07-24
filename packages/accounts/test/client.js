374eeef0c6a7565e7e617ba1a3ea4e08118cf900
Yagiz Nizipli <yagiz@nizipli.com>
2021-07-24 14:54:41 +0300

    tests: add more tests

--- packages/accounts/test/client.js
+++ packages/accounts/test/client.js
@@ -1,12 +1,24 @@
+/* eslint-disable */
+
 import grpc from '@grpc/grpc-js'
 import loader from '@grpc/proto-loader'
 import path from 'path'
+import { promisify } from 'util'
 import config from '../src/config.js'
 
 export const getRandomPort = (a = 1000, b = 65000) => {
   const lower = Math.ceil(Math.min(a, b))
   const upper = Math.floor(Math.max(a, b))
   return Math.floor(lower + Math.random() * (upper - lower + 1))
+}
+
+export function promisifyAll(subscriber) {
+  const to = {}
+  for (const k in subscriber) {
+    if (typeof subscriber[k] != 'function') continue
+    to[k] = promisify(subscriber[k].bind(subscriber))
+  }
+  return to
 }
 
 export function getClients(port = getRandomPort()) {
--- packages/accounts/test/grpc.test.js
+++ packages/accounts/test/grpc.test.js
@@ -1,7 +1,8 @@
 import { randomUUID } from 'crypto'
 import grpc from '@grpc/grpc-js'
+import { promisify } from 'util'
 
-import { getRandomPort, getClients } from './client.js'
+import { getRandomPort, getClients, promisifyAll } from './client.js'
 import logger from '../src/logger.js'
 import app from '../src/grpc.js'
 import pg from '../src/pg/index.js'
@@ -48,6 +49,33 @@ describe('Accounts', () => {
       done()
     })
   })
+
+  test('findAll should return a correct response', (done) => {
+    const identity_id = randomUUID()
+
+    memberships.findOrCreate(
+      { identity_id, name: 'Hello World' },
+      (error, response) => {
+        const account_id = response.account_id
+
+        expect(error).toBeFalsy()
+        expect(response).toBeTruthy()
+        expect(response.account_id).toBeTruthy()
+        expect(response.identity_id).toEqual(identity_id)
+
+        accounts.findAll({ identity_id }, async (error, response) => {
+          expect(error).toBeFalsy()
+          for (const row of response.rows) {
+            expect(row.owner_identity_id).toEqual(identity_id)
+            expect(row.account_id).toEqual(account_id)
+          }
+          await pg.transaction((trx) => Accounts.destroy({ account_id }, trx))
+          done()
+        })
+      },
+    )
+  })
+
   test('update should return error on not found', (done) => {
     accounts.update(
       { account_id: randomUUID(), identity_id: randomUUID(), name: 'hola' },
@@ -56,6 +84,32 @@ describe('Accounts', () => {
         expect(error.code).toEqual(grpc.status.NOT_FOUND)
         done()
       },
+    )
+  })
+
+  test('update should update an account', async () => {
+    const { findOrCreate } = promisifyAll(memberships)
+    const { update, findAll } = promisifyAll(accounts)
+    const identity_id = randomUUID()
+    const membership = await findOrCreate({ identity_id, name: 'hello world' })
+    expect(membership.identity_id).toEqual(identity_id)
+    expect(membership.account_id.length).toBeTruthy()
+
+    await update({
+      account_id: membership.account_id,
+      identity_id,
+      name: 'hola',
+    })
+
+    const {
+      rows: [account],
+    } = await findAll({ identity_id })
+
+    expect(account.name).toEqual('hola')
+    expect(account.account_id).toEqual(membership.account_id)
+    expect(account.owner_identity_id).toEqual(membership.identity_id)
+    await pg.transaction((trx) =>
+      Accounts.destroy({ account_id: membership.account_id }, trx),
     )
   })
 
