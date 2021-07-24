1ae038437dbecb0a3379b33a64b1b3a53cb93aa7
Yagiz Nizipli <yagiz@nizipli.com>
2021-07-24 13:52:53 +0300

    fix: update date parsing on pg side

--- packages/accounts/src/pg/index.js
+++ packages/accounts/src/pg/index.js
@@ -1,5 +1,11 @@
 import knex from 'knex'
-
+import pg from 'pg'
 import config from '../config.js'
+
+const types = pg.types
+
+types.setTypeParser(types.builtins.TIME, (time) => time)
+types.setTypeParser(types.builtins.TIMESTAMP, (time) => time)
+types.setTypeParser(types.builtins.TIMESTAMPTZ, (time) => time)
 
 export default knex(config.knex)
