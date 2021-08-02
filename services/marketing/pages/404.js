5be3d66b32eaeb38ff7f9ec80135ba7fecf61f34
Yagiz Nizipli <yagiz@nizipli.com>
2021-08-02 18:13:34 +0300

    feat: add 404 not found page

---
+++ services/marketing/pages/404.js
@@ -0,0 +1,35 @@
+import Link from 'next/link'
+
+export default function Custom404() {
+  return (
+    <div className="bg-white min-h-screen px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+      <div className="max-w-max mx-auto">
+        <main className="sm:flex">
+          <p className="text-4xl font-extrabold text-orange-500 sm:text-5xl">404</p>
+          <div className="sm:ml-6">
+            <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+              <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                Page not found
+              </h1>
+              <p className="mt-1 text-base text-gray-500">
+                Please check the URL in the address bar and try again.
+              </p>
+            </div>
+            <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+              <Link href="/">
+                <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-orange-500 hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+                  Go back home
+                </a>
+              </Link>
+              <Link href="/company">
+                <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-orange-700 bg-orange-100 hover:bg-orange-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500">
+                  Contact support
+                </a>
+              </Link>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
