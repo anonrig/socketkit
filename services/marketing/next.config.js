b8f3f1d7907d720d4aa8c550dd4ae53a726c62b3
Yagiz Nizipli <yagiz@nizipli.com>
2021-10-15 19:00:43 -0400

    fix: remove locales

--- services/marketing/next.config.js
+++ services/marketing/next.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   poweredByHeader: false,
   reactStrictMode: true,
-  i18n: {
-    locales: ['en-US'],
-    defaultLocale: 'en-US',
-  },
   env: {
     NEXT_PUBLIC_GA_ID: 'G-MP2KX3YPZZ',
   },
