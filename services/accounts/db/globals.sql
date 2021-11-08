d4ff5e3aa9cff8a65053326657de29cb19079590
Yagiz Nizipli <yagiz@nizipli.com>
2021-11-07 19:25:57 -0500

    build(accounts): add workflow configuration

--- services/accounts/db/globals.sql
+++ services/accounts/db/globals.sql
@@ -1,4 +1,4 @@
 CREATE ROLE accounts;
-CREATE ROLE "accounts-worker" LOGIN PASSWORD 'CHANGE-THIS';
+CREATE ROLE "accounts-worker" LOGIN PASSWORD 'change-this';
 
 CREATE DATABASE accounts OWNER accounts TEMPLATE template0 ENCODING "UTF-8" LC_COLLATE "C" LC_CTYPE "C";
