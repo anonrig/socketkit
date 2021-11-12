7c9441e8ac99e6b92c461d48986f531c6e170711
Yagiz Nizipli <yagiz@nizipli.com>
2021-11-12 14:16:10 -0500

    fix(core): fix wrong kratos package reference

--- services/core/src/authentication/kratos.js
+++ services/core/src/authentication/kratos.js
@@ -1,10 +1,10 @@
-import { Configuration, V0alpha1Api } from '@ory/kratos-client'
+import { Configuration, V0alpha2Api } from '@ory/kratos-client'
 import config from '../config.js'
 
-export const kratos = new V0alpha1Api(
+export const kratos = new V0alpha2Api(
   new Configuration({ basePath: config.kratos.public }),
 )
 
-export const kratos_private = new V0alpha1Api(
+export const kratos_private = new V0alpha2Api(
   new Configuration({ basePath: config.kratos.private }),
 )
