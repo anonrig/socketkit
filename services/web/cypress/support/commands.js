01e936b694bb182541a554c3df0cee3540e286cc
Yagiz Nizipli <yagiz@nizipli.com>
2021-11-28 16:12:10 -0500

    fix(web): remove cypress and add eslint-config

--- services/web/cypress/support/commands.js
+++
@@ -1,9 +0,0 @@
-Cypress.Commands.add('login', (user) => {
-  cy.clearCookies()
-  cy.visit('https://web.socketkit.com')
-  cy.get(`input[name='password_identifier']`).type(user.email).should('have.value', user.email)
-  cy.get(`input[name='password']`).type(user.password).should('have.value', user.password)
-  cy.get(`button[value='password']`).click()
-})
-
-Cypress.Commands.add('goToRoot', () => cy.visit('https://web.socketkit.com'))
